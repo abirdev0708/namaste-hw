9e9045f27345a77e4b8d8a924cebf6e27d4145c5
abirdev0708 <abirlalmukherjee786@gmail.com>
2023-03-05 17:43:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const heading1 = React.createElement("h1",{},'Heading 1');
+const heading2 = React.createElement("h1",{},'Heading 2');
+const content = React.createElement("div",{},[heading1,heading2])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(content); 
