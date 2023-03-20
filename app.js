1ae184fbac6d7bb30fa10bcc5be7c2de122a11ef
abirdev0708 <abirlalmukherjee786@gmail.com>
2023-03-21 00:20:29 +0530

    Introducing JSX Data

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-const heading1 = React.createElement("h1",{
-    id: "heading1",
-    key: "heading1",
-},'Heading 3');
-const heading2 = React.createElement("h1",{
-    id: "heading2",
-    key: "heading2",
-},'Heading 2');
-const content = React.createElement("div",{
-    id: "content",
-    key: "content",
-},[heading1,heading2]);
+import ReactDOM from 'react-dom/client';
+const Title =()=> <h1>Abir Fruit Shop</h1>;
+const SubTitle = <h1>List Here</h1>
+const MainData = () => {
+    return <div>
+        <h2>Fruit</h2>
+        <ul>
+            <li>Banana</li>
+            <li>Pinapple</li>
+            <li>Mango</li>
+        </ul>
+    </div>
+}
+const Content =()=> {
+    return <div>
+        <Title/>
+        {SubTitle}
+        <MainData/>
+    </div>};
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(content); 
+root.render(<Content/>); 
