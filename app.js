1cd55a3db813a2431abea1a85f1bb7157815a1d1
abirdev0708 <abirlalmukherjee786@gmail.com>
2023-03-20 23:59:37 +0530

    Modification in the package section

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const heading1 = React.createElement("h1",{},'Heading 1');
-const heading2 = React.createElement("h1",{},'Heading 2');
-const content = React.createElement("div",{},[heading1,heading2])
+const heading1 = React.createElement("h1",{
+    id: "heading1",
+    key: "heading1",
+},'Heading 3');
+const heading2 = React.createElement("h1",{
+    id: "heading2",
+    key: "heading2",
+},'Heading 2');
+const content = React.createElement("div",{
+    id: "content",
+    key: "content",
+},[heading1,heading2]);
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(content); 
